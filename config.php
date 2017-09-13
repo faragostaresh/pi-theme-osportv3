14fbc5dc305bbc8e4f73cbe069f3b8fa0ca9a7bf
voltan <djvoltan@gmail.com>
2017-09-13 15:48:50 +0430

    Work on theme

---
+++ config.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt BSD 3-Clause License
+ */
+return array(
+    /**#@+
+     * To be stored in DB
+     */
+    // Version
+    'version'       => '1.0.0',
+    // Type of layouts available in the theme
+    // Potential value: 'both', 'admin', 'front', default as 'both'
+    'type'          => 'front',
+    /**#@-**/
+
+    // Title of the theme
+    'title'         => 'oSport version 3 Theme',
+    // Author information: name, email, website
+    'author'        => '',
+    // Screenshot image, relative path in asset.
+    // If no screenshot is available, static/image/screenshot.png will be used
+    'screenshot'    => 'image/screenshot.png',
+    // License or theme images and scripts
+    'license'       => 'Creative Common License'
+        . ' http://creativecommons.org/licenses/by/3.0/',
+    // Optional description
+    'description'   => 'oSport version 3 Theme',
+    // Parent theme from which templates can be inherited, default as 'default'
+    'parent'        => '',
+    // Supported browsers
+    'browser'       => 'Internet Explorer: 6+; Chrome: 17+; Firefox: 10+;'
+        . ' Safari: 5.1+; Opera: 9.8+;',
+
+    // List of custom front layouts, optional
+    'layout'        => array(
+        //'file-name'   => __('Layout name'),
+    ),
+);
